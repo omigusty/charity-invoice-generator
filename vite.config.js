47124b8e1e0806d9aee0566bb28bb00acf489f87
omigusty <omistyfani@gmail.com>
2023-09-25 08:58:30 +0700

    change css

--- vite.config.js
+++ vite.config.js
@@ -2,50 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 
-// const manifestForPlugIn = {
-//   registerType: "prompt",
-//   includeAssests: ["favicon.ico", "apple-touc-icon.png", "masked-icon.svg"],
-//   manifest: {
-//     name: "Invoice Generator",
-//     short_name: "invoice-generator",
-//     description: "Aplikasi pembuatan bukti penerimaan shadaqah otomatis",
-//     icons: [
-//       {
-//         src: "/android-chrome-192x192.png",
-//         sizes: "192x192",
-//         type: "image/png",
-//         purpose: "favicon",
-//       },
-//       {
-//         src: "./public/android-chrome-512x512.png",
-//         sizes: "512x512",
-//         type: "image/png",
-//         purpose: "favicon",
-//       },
-//       {
-//         src: "./public/apple-touch-icon.png",
-//         sizes: "180x180",
-//         type: "image/png",
-//         purpose: "apple touch icon",
-//       },
-//       {
-//         src: "./public/maskable_icon.png",
-//         sizes: "512x512",
-//         type: "image/png",
-//         purpose: "any maskable",
-//       },
-//     ],
-//     theme_color: "#171717",
-//     background_color: "#f0e7db",
-//     display: "standalone",
-//     scope: "/",
-//     start_url: "/",
-//     orientation: "portrait",
-//   },
-// };
-
 export default defineConfig({
-  // plugins: [react(), VitePWA(manifestForPlugIn)],
   plugins: [
     react(),
     VitePWA({
